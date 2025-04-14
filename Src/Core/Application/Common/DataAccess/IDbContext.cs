668830454201be245945b33d0d2c5a62dad5e428
Emmanuel Elizondo <eelizondo@coopedota.com>
2025-04-14 16:36:56 -0600

    feat(application): agrega interfaz para repositorio de estados e incorpora en interfaz de DbContext

--- Src/Core/Application/Common/DataAccess/IDbContext.cs
+++ Src/Core/Application/Common/DataAccess/IDbContext.cs
@@ -1,7 +1,14 @@
 namespace Application.Common.DataAccess;
 
+/// <summary>
+/// Clase de contexto de la base de datos.
+/// </summary>
 public interface IDbContext
 {
+    /// <summary>
+    /// Repositorio para las solicitudes.
+    /// </summary>
     ISolicitudRepository SolicitudRepository { get; }
 
+    IEstadoRepository EstadoRepository { get; }
 }
---
+++ Src/Core/Application/Common/DataAccess/IEstadoRepository.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Entidades;
+
+namespace Application.Common.DataAccess
+{
+    /// <summary>
+    /// Interfaz para el repositorio de estados de solicitudes.
+    /// </summary>
+    public interface IEstadoRepository
+    {
+        /// <summary>
+        /// Obtiene una lista con los estados.
+        /// </summary>
+        /// <returns>Lista con los estados en base de datos.</returns>
+        Task<List<Estado>> GetEstados();
+    }
+}

af09fe9762722f97961ccf2b4a8efc3fd2131c3b
Emmanuel Elizondo <eelizondo@coopedota.com>
2025-04-14 16:43:53 -0600

    feat(infraestructure): agrega clase SQLQuerys a utilizar con Dapper

---
+++ Src/Infrastructure/DataAccess/SQLQuerys.cs
@@ -0,0 +1,37 @@
+﻿namespace Application.Common
+{
+    /// <summary>
+    /// Clase con querys a ejecutar en base de datos con Dapper.
+    /// </summary>
+    public class SQLQuerys
+    {
+        /// <summary>
+        /// Query para obtener solicitudes filtradas y paginadas.
+        /// </summary>
+        public const string GetSolicitudesFiltradas = @"
+            DECLARE @Solicitudes TABLE (Id INT, FechaSolicitud DATE, Solicitante VARCHAR(100), IdEstado INT, Estado VARCHAR(50));
+            INSERT INTO @Solicitudes
+            EXEC [dbo].[GetSolicitudes]
+
+            SELECT
+                Id,
+                FechaSolicitud,
+                Solicitante,
+                IdEstado,
+                Estado AS EstadoValue
+            FROM @Solicitudes
+            {0}
+            {1}
+        ";
+
+        /// <summary>
+        /// Query para obtener los estados registrados en base de datos.
+        /// </summary>
+        public const string GetEstados = @"
+            SELECT
+                IdEstado,
+                Estado
+            FROM [dbo].[Estados]
+        ";
+    }
+}

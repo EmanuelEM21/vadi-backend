9508a53485df17b0f34f74ed0134240916f91861
Emmanuel Elizondo <eelizondo@coopedota.com>
2025-04-15 09:31:15 -0600

    feature(WebAPI): implementa controladores de solicitudes y estados

---
+++ Src/WebApi/Controllers/EstadoController.cs
@@ -0,0 +1,32 @@
+﻿using Application.Common.DataAccess;
+using Domain.Entidades;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EstadoController : ControllerBase
+    {
+        private readonly IDbContext dbContext;
+
+        public EstadoController(IDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet("obtener-estados")]
+        public async Task<IActionResult> GetEstados()
+        {
+            try
+            {
+                var listaEstados = await dbContext.EstadoRepository.GetEstados() ?? new List<Estado>();
+                return Ok(listaEstados);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Hubo un error durante la obtención de los datos: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ Src/WebApi/Controllers/SolicitudController.cs
@@ -0,0 +1,80 @@
+﻿using Application.Common.DataAccess;
+using Domain.Entidades;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SolicitudController : ControllerBase
+    {
+        private readonly IDbContext dbContext;
+
+        public SolicitudController(IDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpPost("crear-solicitud")]
+        public async Task<IActionResult> CreateSolicitud(Solicitud solicitud)
+        {
+            try
+            {
+                var resultado = await dbContext.SolicitudRepository.InsertarSolicitud(solicitud);
+                return resultado
+                    ? Created("Solicitud creada correctamente", "")
+                    : BadRequest("No se pudo registrar la solicitud");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Hubo un error durante el proceso de registro: {ex.Message}");
+            }
+        }
+
+        [HttpPost("obtener-solicitudes")]
+        public async Task<IActionResult> GetSolicitudes(Filtro filtro)
+        {
+            try
+            {
+                var listaSolicitudes = await dbContext.SolicitudRepository.GetSolicitudes(filtro) ?? new List<Solicitud>();
+                return Ok(listaSolicitudes);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Hubo un error durante la obtención de los datos: {ex.Message}");
+            }
+        }
+
+        [HttpPut("actualizar-solicitud")]
+        public async Task<IActionResult> UpdateSolicitud(Solicitud solicitud)
+        {
+            try
+            {
+                var resultado = await dbContext.SolicitudRepository.UpdateSolicitud(solicitud);
+                return resultado
+                    ? Ok("Solicitud actualizada correctamente")
+                    : BadRequest("No se pudo actualizar la solicitud");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Hubo un error durante el proceso de actualización: {ex.Message}");
+            }
+        }
+
+        [HttpDelete("eliminar-solicitud")]
+        public async Task<IActionResult> DeleteSolicitud(int solicitudId)
+        {
+            try
+            {
+                var resultado = await dbContext.SolicitudRepository.DeleteSolicitud(solicitudId);
+                return resultado
+                    ? Ok("Solicitud eliminada correctamente")
+                    : BadRequest("No se pudo eliminar la solicitud");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Hubo un error durante el proceso de actualización: {ex.Message}");
+            }
+        }
+    }
+}

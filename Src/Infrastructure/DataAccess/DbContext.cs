577df241ba025f07977f8a13a81367132818c8b0
Emmanuel Elizondo <eelizondo@coopedota.com>
2025-04-15 08:06:03 -0600

    feat(infraestructure): agrega clase DbContext para manejo de repositorios

---
+++ Src/Infrastructure/DataAccess/DbContext.cs
@@ -0,0 +1,24 @@
+﻿using Application.Common.DataAccess;
+
+namespace DataAccess
+{
+    /// <summary>
+    /// Clase de contexto para la base de datos.
+    /// </summary>
+    public class DbContext : IDbContext
+    {
+        public ISolicitudRepository SolicitudRepository { get; }
+        public IEstadoRepository EstadoRepository { get; }
+
+        /// <summary>
+        /// Constructor de la clase DbContext.
+        /// </summary>
+        /// <param name="solicitudRepository">Repositorio de solicitudes.</param>
+        /// <param name="estadoRepository">Repositorio de estados de solicitudes.</param>
+        public DbContext(ISolicitudRepository solicitudRepository, IEstadoRepository estadoRepository)
+        {
+            SolicitudRepository = solicitudRepository;
+            EstadoRepository = estadoRepository;
+        }
+    }
+}
